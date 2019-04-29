5cf61041b5292376e7c74d509957fc09d4835131
Alex <alex7chiaster@gmail.com>
2019-04-29 10:23:13 +0800

    Updates

---
+++ precache-manifest.40cedd6bfe63c6040962efbefb94e908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57baa19da840c4bff56c1514db5e39e0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f6fe1c1550d4f8efbbaf",
+    "url": "/robofriends/static/css/main.ec93a582.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "f6fe1c1550d4f8efbbaf",
+    "url": "/robofriends/static/js/main.68dce418.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
